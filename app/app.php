b43524a44840611e55638b505d8a04097808514a
Tim White <fignewtonfood@gmail.com>
2015-08-07 09:36:43 -0700

    Adds constructor and properties to Address.php

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
+  //dependencies
   //opens autoload.php which loads Silex files in vendor folder
     require_once __DIR__."/../vendor/autoload.php";
-  //opens Address.php  
+  //opens Address.php
     require_once __DIR__."/../src/Address.php";
 ?>
---
+++ src/Address.php
@@ -0,0 +1,24 @@
+<?php
+  //establish Address class
+    class Address
+    {
+      //setting properties to private
+        private $name;
+        private $phone;
+        private $street;
+        private $city;
+        private $state;
+        private $zip;
+
+  //constructor for Car class
+    function __construct()
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->street = $street;
+        $this->city = $city;
+        $this->state = $state;
+        $this->zip = $zip;
+    }
+    }
+?>
