d708c26e0cdcc9f571d1f009661065de1672fb7f
Tim White <fignewtonfood@gmail.com>
2015-08-07 09:31:00 -0700

    Adds dependencies to app.php

---
+++ app/app.php
@@ -0,0 +1,6 @@
+<?php
+  //opens autoload.php which loads Silex files in vendor folder
+    require_once __DIR__."/../vendor/autoload.php";
+  //opens Address.php  
+    require_once __DIR__."/../src/Address.php";
+?>
